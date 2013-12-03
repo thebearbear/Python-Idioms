e5adfa6811051ec0e31f813f40ee6645ad95609d
odeits <odeits@gmail.com>
2013-12-02 22:27:54 -0800

    grouper
    the itertools implementation is consistently faster than naive yield

--- blocks_from_sequence.py
+++ blocks_from_sequence.py
@@ -1,3 +1,12 @@
 def chunker(seq, size):
     for i in range(0, len(seq), size):
       yield s[i:i+size] 
+
+
+# taken from http://docs.python.org/2.7/library/itertools.html#recipes
+from itertools import izip_longest
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
